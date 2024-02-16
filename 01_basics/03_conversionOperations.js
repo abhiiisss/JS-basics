4fdd411dcde123bde7fb19b00743347421d9385a
Abhishek Sharma <91886058+abhis12q@users.noreply.github.com>
2024-02-16 12:53:41 +0000

    Conversion Operatrions

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -11,11 +11,47 @@
 // true => 1; false => 0
 
 
-let IsLoggedIn = "abhis"
+// let IsLoggedIn = "abhis"
 
-let booleanIsLoggedIn = Boolean(IsLoggedIn)
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(IsLoggedIn)
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
 // "abhis" => true
+
+
+
+//**************operations************//
+
+let value = 3
+let negvalue = -value
+
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = " Abhis"
+
+let str3 = str1 + str2;
+
+// console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+
+let gameCounter = 100
+gameCounter++;
+
+console.log(gameCounter);
