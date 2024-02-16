f54db876c0af90f758a21483c7f2e6d37061f556
Abhishek Sharma <91886058+abhis12q@users.noreply.github.com>
2024-02-16 13:19:01 +0000

    Comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+//Avoid this type of codes 
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+
+
+// === strict comparison, it compares the datatypes of the value and then gives the output
+
+console.log("2" === 2);
