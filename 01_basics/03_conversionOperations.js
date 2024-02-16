ff1df64d145c5afe8f1809b4de07b3e1bc0a72e9
Abhishek Sharma <91886058+abhis12q@users.noreply.github.com>
2024-02-16 09:41:55 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,21 @@
+// let score = "abhis"
+
+// console.log(typeof score);
+
+// let valueINumber = Number(score)
+// console.log(typeof valueINumber);
+// console.log(valueINumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+
+let IsLoggedIn = "abhis"
+
+let booleanIsLoggedIn = Boolean(IsLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "abhis" => true
