46a3454b40331d0f80f035463d973daf2279519a
Abhishek Sharma <91886058+abhis12q@users.noreply.github.com>
2024-02-21 11:23:18 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -6,7 +6,7 @@ function sayMyName(){
     console.log("H");
     console.log("I");
     console.log("S");
-} // a set if codes which we had to execute repedeatedly
+} // a set o f codes which we had to execute repedeatedly
 
 // sayMyName()
 
@@ -38,7 +38,7 @@ function loginUserMessage(username){
 
 function loginUserMessage(username){
     if(username === undefined){
-         console.log("Please enter a UserName");
+        //  console.log("Please enter a UserName");
          return
     }
     return `${username} just logged in`
@@ -47,19 +47,65 @@ function loginUserMessage(username){
 
 function loginUserMessage(username){
     if(!username){ // same as username === uundefined
-         console.log("Please enter a UserName");
+        //  console.log("Please enter a UserName");
          return
     }
     return `${username} just logged in`
 }
 
-function loginUserMessage(username = "ajay") //It will never let the output be empty
+function loginUserMessage(username = "ajay"){ //It will never let the output be empty
     if(!username){
-         console.log("Please enter a UserName");
+        //  console.log("Please enter a UserName");
          return
     }
     return `${username} just logged in`
 }
 
-console.log(loginUserMessage("Anil")); // overwrites the current code
+//console.log(loginUserMessage("Anil")); // overwrites the current code
+
+// Rest Operator ...
+
+function calculateMyCart (...num1){ // here the three dots represent the rest operator which returns all arguments in the output as a string
+    return num1
+}
+
+console.log(calculateMyCart(200,300,400));
+
+
+function calculateCart (val1, val2, ...num2){ // here the three dots represent the rest operator which returns all arguments in the output as a string
+    return num2
+}
+
+ console.log(calculateCart(200,300,400,2000));
+ // here the val1 and val2 does not comes in output as the comes before the num2 in rest operator 
+
+ const user = {
+    username : "abhis",
+    age : 22
+
+ }
+
+ function handleObject(anyObject){
+        console.log(`Username is ${anyObject.username} and his age is ${anyObject.age}`);
+ }
+
+
+// handleObject(user) //passing objects by creating variables
+
+//handleObject({ //passing objects directly
+   // username: "random",
+    //age: 27
+// })
+
+//creating an array and a function to pass an array
+const myArray = [20,30,40,50]
+
+function handleArray (arrayHandling){
+    return arrayHandling[1]
+}
+
+//console.log(handleArray(myArray)); //passing arrays using variables
+
+console.log(handleArray([200,400,500,100]));
+
 
