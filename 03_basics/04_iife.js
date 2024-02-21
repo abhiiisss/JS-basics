bd24f17fe8b13bfabde5ee760d1b1df5cefc76e5
Abhishek Sharma <91886058+abhis12q@users.noreply.github.com>
2024-02-21 14:42:06 +0000

    iife

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,22 @@
+// Immediately Invoked Function Expressions(IIFE)
+
+(function db (){ //named IIFE
+    console.log('Database Connected');
+}) ();
+
+
+// () () -- the first bracket represents here paranthesis for function and the second bracket represents the execution of that function, we can see that in the above code --iife
+
+// we use iife to remove the pollution of global scope
+
+// we end the invoked iife using semicolon
+
+( () => { 
+console.log('connection successfull');
+})(); //iife with arrow function
+
+
+( (name) => { //Unnamed IIFE
+console.log(`connection successfull, ${name}`);
+})("abhis"); 
+//iife with arrow function here we have given arguments and parametersz
