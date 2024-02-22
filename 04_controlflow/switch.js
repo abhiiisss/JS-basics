7650919d9df5a31e59ba6da55ac1f15cfa4b0a28
Abhishek Sharma <91886058+abhis12q@users.noreply.github.com>
2024-02-22 16:36:56 +0000

    base switch

---
+++ 04_controlflow/switch.js
@@ -0,0 +1,35 @@
+// getting used to switch case
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// } /switch's syntax
+
+
+//using switch case to ignore multiple else-if conditions
+const month = "mar"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+
+    case "feb":
+        console.log("February");
+        break;
+
+    case "mar":
+        console.log("March");
+        break;
+
+    case "apr":
+        console.log("April");
+        break;
+
+    default:
+        break;
+}
