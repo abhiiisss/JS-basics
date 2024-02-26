399a03bafcc17d8aed5c148e7afd4fbcf0cd5ba0
Abhishek Sharma <91886058+abhiiisss@users.noreply.github.com>
2024-02-26 11:39:28 +0000

    do-while

---
+++ 05_iterations/two.js
@@ -0,0 +1,31 @@
+/// while
+// i = 0;
+// while (i < 10) {
+//    console.log(`the value is ${i} `)
+//     i = i+2
+// }
+
+
+let i = ["A", "B", "C", "D"]
+let j = 0
+ while (j < i.length ) {
+    //console.log(`value is ${i[j]}`);
+    j++;
+ }
+
+ /// do while
+
+let score = 10
+
+ do {
+    //console.log(`Score is ${score}`);
+    score++
+ } while (score <=10);
+
+
+let sore = 11
+
+ do {
+    console.log(`Score is ${sore}`);
+    sore++
+ } while (sore <= 10);
